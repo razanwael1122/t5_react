7cd37518f25aa7aebaf6a20ce14411007a1865c4
razanwael1122 <razanwael1122@gmail.com>
2023-07-11 12:48:37 +0300

    commit1

--- App.js
+++ App.js
@@ -6,7 +6,6 @@ import {
   Text,
   StyleSheet,
   Platform,
-  ScrollView,
 } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import * as MediaLibrary from "expo-media-library";
@@ -42,7 +41,7 @@ class MergeImages extends React.Component {
       }
     }
   };
-  //////////////////////////////////////////////////////
+
   openCamera = async () => {
     let result;
     try {
@@ -69,7 +68,7 @@ class MergeImages extends React.Component {
       }
     }
   };
-  //////////////////////////////////////////////////////////
+
   deleteImage = () => {
     const { selectedImageIndex } = this.state;
     this.setState((prevState) => {
@@ -83,33 +82,113 @@ class MergeImages extends React.Component {
       };
     });
   };
-  ////////////////////////////////////////////////////////
+
   mergeImages = async () => {
     const { images } = this.state;
-    if (images.length < 4) {
-      alert("Please capture 4 images first!");
+    if (images.length < 10) {
+      alert("Please capture 10 images first!");
+      return;
+    }
+
+    if (images.length > 10) {
+      alert("Cannot merge more than 10 images!");
       return;
     }
 
     try {
-      const image1 = { uri: images[0] };
-      const image2 = { uri: images[1] };
+      const mergedImage = await this.merge(images); // Merge all captured images
+      await this.saveMergedImage(mergedImage);
+      alert("Images merged and saved successfully!");
+    } catch (error) {
+      // Handle error
+      //console.log("Error merging images:", error);
+    }
+  };
 
+  merge = async (images) => {
+    try {
+      const processedImages = await Promise.all(
+        images.map(async (imageUri) => {
+          const { width, height } = await Image.getSizeAsync(imageUri);
+          return { uri: imageUri, width, height };
+        })
+      );
+
+      const maxWidth = Math.max(...processedImages.map((img) => img.width));
+      const totalHeight = processedImages.reduce(
+        (sum, img) => sum + img.height,
+        0
+      );
+
+      const canvasWidth = maxWidth;
+      const canvasHeight = totalHeight;
+
+      const canvas = document.createElement("canvas");
+      canvas.width = canvasWidth;
+      canvas.height = canvasHeight;
+      const ctx = canvas.getContext("2d");
+
+      let offsetY = 0;
+      for (const { uri, width, height } of processedImages) {
+        const image = { uri };
+        const { uri: processedUri } = await ImageManipulator.manipulateAsync(
+          [image],
+          [{ resize: { width, height } }],
+          { format: "png" }
+        );
+        const img = new Image();
+        img.src = processedUri;
+        await new Promise((resolve) => {
+          img.onload = resolve;
+        });
+        const x = (canvasWidth - width) / 2;
+        ctx.drawImage(img, x, offsetY, width, height);
+        offsetY += height;
+      }
+
+      const mergedImage = canvas.toDataURL("image/png");
+      return mergedImage;
+    } catch (error) {
+      // Handle error
+      // console.log("Error merging images:", error);
+    }
+  };
+
+  mergeHorizontally = async (images) => {
+    try {
       const mergedImage = await ImageManipulator.manipulateAsync(
-        [image1, image2],
-        [{ resize: { width: image1.width } }],
+        images,
+        [{ concatenate: { axis: "horizontal" } }],
         { format: "png" }
       );
 
       return mergedImage.uri;
-    } catch (error) {}
+    } catch (error) {
+      // console.log("Error merging images horizontally:", error);
+      // throw error;
+    }
   };
-  /////////////////////////////////////////////////////////////
+
+  mergeVertically = async (image1, image2) => {
+    try {
+      const mergedImage = await ImageManipulator.manipulateAsync(
+        [{ uri: image1 }, { uri: image2 }],
+        [{ concatenate: { axis: "vertical" } }],
+        { format: "png" }
+      );
+
+      return mergedImage.uri;
+    } catch (error) {
+      // console.log("Error merging images vertically:", error);
+      //throw error;
+    }
+  };
+
   saveMergedImage = async (image) => {
     try {
       const mergedRef = this.mergeRef.current;
       await new Promise((resolve) => {
-        setTimeout(resolve, 40);
+        setTimeout(resolve, 10);
       });
 
       const uri = await captureRef(mergedRef, {
@@ -120,37 +199,45 @@ class MergeImages extends React.Component {
       const asset = await MediaLibrary.createAssetAsync(uri);
       await MediaLibrary.saveToLibraryAsync(asset);
       alert("Images merged and saved successfully!");
-    } catch (error) {}
+    } catch (error) {
+      //console.log("Error saving merged image:", error);
+      //throw error;
+    }
   };
-  //////////////////////////////////////////////////////////
+
   saveImageToGallery = async (imageUri) => {
     try {
       const asset = await MediaLibrary.createAssetAsync(imageUri);
       await MediaLibrary.saveToLibraryAsync(asset);
-    } catch (error) {}
+      console.log("Saved image URI:", asset.uri);
+    } catch (error) {
+      //console.log("Error saving image to gallery:", error);
+    }
   };
-  ///////////////////////////////////////////////////////////////
+
   openImage = (imageUri, index) => {
     this.setState({
       selectedImage: imageUri,
       selectedImageIndex: index,
     });
   };
-  //////////////////////////////////////////////
+
   render() {
     const { images, selectedImage } = this.state;
+
+    // Divide images into two rows
+    const firstRowImages = images.slice(0, 5);
+    const secondRowImages = images.slice(5, 10);
 
     return (
       <View style={styles.container}>
         <TouchableOpacity style={styles.cameraButton} onPress={this.openCamera}>
           <Text style={styles.buttonText}>Capture Image</Text>
         </TouchableOpacity>
-        <ScrollView
-          style={styles.scrollView}
-          contentContainerStyle={styles.contentContainer}
-          horizontal={true}
-        >
-            {images.map((image, index) => (
+        <View style={styles.imageContainer} ref={this.mergeRef}>
+          {/* First Row */}
+          <View style={styles.rowContainer}>
+            {firstRowImages.map((image, index) => (
               <View key={index} style={styles.imageWrapper}>
                 <TouchableOpacity onPress={() => this.openImage(image, index)}>
                   <Image
@@ -160,7 +247,26 @@ class MergeImages extends React.Component {
                   />
                 </TouchableOpacity>
               </View>
-        </ScrollView>
+            ))}
+          </View>
+
+          {/* Second Row */}
+          <View style={styles.rowContainer}>
+            {secondRowImages.map((image, index) => (
+              <View key={index} style={styles.imageWrapper}>
+                <TouchableOpacity
+                  onPress={() => this.openImage(image, index + 5)}
+                >
+                  <Image
+                    source={{ uri: image }}
+                    style={styles.image}
+                    resizeMode="cover"
+                  />
+                </TouchableOpacity>
+              </View>
+            ))}
+          </View>
+        </View>
         <TouchableOpacity style={styles.mergeButton} onPress={this.mergeImages}>
           <Text style={styles.buttonText}>Merge Images</Text>
         </TouchableOpacity>
@@ -189,18 +295,22 @@ class MergeImages extends React.Component {
     );
   }
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
+    padding: 10,
     justifyContent: "center",
     alignItems: "center",
   },
-  scrollView: {
-    marginVertical: 10,
-    maxHeight: 200,
-  },
+
   contentContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  rowContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
     alignItems: "center",
   },
   cameraButton: {
@@ -208,7 +318,7 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 5,
     marginRight: 10,
-    bottom: 50,
+    marginBottom: 10,
   },
   mergeButton: {
     backgroundColor: "gray",
@@ -220,17 +330,24 @@ const styles = StyleSheet.create({
     color: "white",
     fontWeight: "bold",
   },
-  imageWrapper: {
-    width: 200,
-    height: 200,
+  imageContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+
+  image: {
+    width: 70,
+    height: 90,
     borderRadius: 1,
-    margin: 1,
   },
   deleteButton: {
     position: "absolute",
-    top: 25,
-    padding: 10,
-    left: 20,
+    top: 33,
+    left: 5,
+    backgroundColor: "red",
+    padding: 5,
+    borderRadius: 5,
+    zIndex: 1,
   },
   deleteButtonText: {
     color: "white",
